ed89d3629983b97546ac9e7db3584a58f4f98bad
@小小只^v^ <littlezov@qq.com>
2021-04-30 15:56:50 +0800

    style:fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 
 curl_put()
 {
-    RET=`/usr/bin/curl -s -w '%{http_code}' -X PUT --data-binary @$1 --unix-socket /var/run/control.unit.sock http://localhost/$2`
+    RET=$(/usr/bin/curl -s -w '%{http_code}' -X PUT --data-binary @"$1" --unix-socket /var/run/control.unit.sock http://localhost/"$2")
     RET_BODY=${RET::-3}
-    RET_STATUS=$(echo $RET | /usr/bin/tail -c 4)
+    RET_STATUS=$(echo "$RET" | /usr/bin/tail -c 4)
     if [ "$RET_STATUS" -ne "200" ]; then
         echo "$0: Error: HTTP response status code is '$RET_STATUS'"
         echo "$RET_BODY"
@@ -18,7 +18,7 @@ curl_put()
     return 0
 }
 
-if [ "$1" = "unitd" -o "$1" = "unitd-debug" ]; then
+if [ "$1" = "unitd" ] || [ "$1" = "unitd-debug" ]; then
     if /usr/bin/find "/var/lib/unit/" -mindepth 1 -print -quit 2>/dev/null | /bin/grep -q .; then
         echo "$0: /var/lib/unit/ is not empty, skipping initial configuration..."
     else
@@ -34,19 +34,19 @@ if [ "$1" = "unitd" -o "$1" = "unitd-debug" ]; then
             echo "$0: Looking for certificate bundles in /docker-entrypoint.d/..."
             for f in $(/usr/bin/find /docker-entrypoint.d/ -type f -name "*.pem"); do
                 echo "$0: Uploading certificates bundle: $f"
-                curl_put $f "certificates/$(basename $f .pem)"
+                curl_put "$f" "certificates/$(basename "$f" .pem)"
             done
 
             echo "$0: Looking for configuration snippets in /docker-entrypoint.d/..."
             for f in $(/usr/bin/find /docker-entrypoint.d/ -type f -name "*.json"); do
                 echo "$0: Applying configuration $f";
-                curl_put $f "config"
+                curl_put "$f" "config"
             done
 
             echo "$0: Looking for shell scripts in /docker-entrypoint.d/..."
             for f in $(/usr/bin/find /docker-entrypoint.d/ -type f -name "*.sh"); do
                 echo "$0: Launching $f";
-                "$f"
+                ."$f";
             done
 
             # warn on filetypes we don't know what to do with
@@ -55,7 +55,7 @@ if [ "$1" = "unitd" -o "$1" = "unitd-debug" ]; then
             done
 
             echo "$0: Stopping Unit daemon after initial configuration..."
-            kill -TERM `/bin/cat /var/run/unit.pid`
+            kill -TERM "$(/bin/cat /var/run/unit.pid)"
 
             while [ -S /var/run/control.unit.sock ]; do echo "$0: Waiting for control socket to be removed..."; /bin/sleep 0.1; done
 
